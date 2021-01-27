3b73d4fc46f1478561ed819319043841b68ad258
dmdixon <47756213+dmdixon@users.noreply.github.com>
2021-01-26 21:48:59 -0600

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -12,8 +12,12 @@ from stats import *
 from plots import *
 
 if __name__=='__main__':
-    
-    starttime=datetime.now(pytz.timezone(config.timezone))
+    if config.query_date==None:
+        query_date=datetime.now(pytz.timezone(config.timezone))
+        starttime = query_date
+    else:
+        query_date = pytz.timezone(config.timezone).localize(datetime.strptime(config.query_date,'%m/%d/%Y'))
+        starttime = datetime.now(pytz.timezone(config.timezone))
     if config.Web_Scrape=='Y':
         try:
             #__Web_Scraping__#
@@ -22,11 +26,11 @@ if __name__=='__main__':
             aai=Query_All_Active_Inmates(query_clerk=config.query_clerk)
             
             if isinstance(rb,pd.DataFrame)==True:
-                rb.to_csv(r'../data/recent_bookings_'+starttime.strftime('%b_%d_%Y')+'.csv',index=False)
+                rb.to_csv(r'../data/recent_bookings_'+query_date.strftime('%b_%d_%Y')+'.csv',index=False)
             if isinstance(aai,pd.DataFrame)==True:
-                aai.to_csv(r'../data/active_inmates_'+starttime.strftime('%b_%d_%Y')+'.csv',index=False)
+                aai.to_csv(r'../data/active_inmates_'+query_date.strftime('%b_%d_%Y')+'.csv',index=False)
         except Exception as e:
-            Error_Log('Web Scraping',traceback.format_exc(),starttime)
+            Error_Log('Web Scraping',traceback.format_exc(),query_date)
             raise e
     elif config.Web_Scrape=='N':
         rb=None
@@ -35,7 +39,7 @@ if __name__=='__main__':
         
     else:
         error='No Y/N indicated whether to Web Scrape in config file. Program killed, please check config.py...'
-        Error_Log('Web Scraping',error,starttime)
+        Error_Log('Web Scraping',error,query_date)
         raise Exception(error)
     
     
@@ -47,9 +51,9 @@ if __name__=='__main__':
             #__Calculating_Statistics__#
             print('Solving Relevant Statistics...\n')
             if isinstance(rb,pd.DataFrame)==False:
-                rb=pd.read_csv('../data/recent_bookings_'+starttime.strftime('%b_%d_%Y')+'.csv')
+                rb=pd.read_csv('../data/recent_bookings_'+query_date.strftime('%b_%d_%Y')+'.csv')
             if isinstance(aai,pd.DataFrame)==False:
-                aai=pd.read_csv('../data/active_inmates_'+starttime.strftime('%b_%d_%Y')+'.csv')
+                aai=pd.read_csv('../data/active_inmates_'+query_date.strftime('%b_%d_%Y')+'.csv')
             
             aai_num=['Age','Total Bond','Number of Charges','Number of Misd','Number of Felonies']
             
@@ -61,14 +65,14 @@ if __name__=='__main__':
             
             ANOVA_1Way_by_Race_Sex(aai,aai_num,'../tables/anova.tex')
         except Exception as e:
-            Error_Log('Calculating Statistics',traceback.format_exc(),starttime,r'../misc/error.txt')
+            Error_Log('Calculating Statistics',traceback.format_exc(),query_date,r'../misc/error.txt')
             raise e
             
     elif config.Calc_Stats=='N':
         print('Skipping Calculation of Statistics...\n')
     else:
         error='No Y/N indicated whether to Calc Stats in config file. Killing program, please check config.py...'
-        Error_Log('Calculating Statistics',error,starttime,r'../misc/error.txt')
+        Error_Log('Calculating Statistics',error,query_date,r'../misc/error.txt')
         raise Exception(error)
     
     
@@ -80,7 +84,7 @@ if __name__=='__main__':
             #__Plotting_Distributions__#
             print('Plotting Parameter Distributions...\n')
             if isinstance(aai,pd.DataFrame)==False:
-                aai=pd.read_csv('../data/active_inmates_'+starttime.strftime('%b_%d_%Y')+'.csv')
+                aai=pd.read_csv('../data/active_inmates_'+query_date.strftime('%b_%d_%Y')+'.csv')
             
             #__All_Active_Inmates__#
             Plot_Bar(aai,'Facility','Number of Inmates',savepath='../plots/Facilities.pdf')
@@ -92,14 +96,14 @@ if __name__=='__main__':
             Plot_Race_Pies_by_Sex(aai,'../plots/')
             
         except Exception as e:
-            Error_Log('Generating Plots',traceback.format_exc(),starttime,r'../misc/error.txt')
+            Error_Log('Generating Plots',traceback.format_exc(),query_date,r'../misc/error.txt')
             raise e
             
     elif config.Plot_Dists=='N':
         print('Skipping Plot Generation...\n')
     else:
         error='No Y/N indicated whether to Plot Dists in config file. Killing program, please check config.py...'
-        Error_Log('Generating Plots',error,starttime,r'../misc/error.txt')
+        Error_Log('Generating Plots',error,query_date,r'../misc/error.txt')
         raise Exception(error)
         
         
@@ -109,8 +113,10 @@ if __name__=='__main__':
     if config.Gen_Report=='Y':
         try:
             print('Generating Report PDF...\n')
+            if isinstance(rb,pd.DataFrame)==False:
+                rb=pd.read_csv('../data/recent_bookings_'+query_date.strftime('%b_%d_%Y')+'.csv')
             if isinstance(aai,pd.DataFrame)==False:
-                aai=pd.read_csv('../data/active_inmates_'+starttime.strftime('%b_%d_%Y')+'.csv')
+                aai=pd.read_csv('../data/active_inmates_'+query_date.strftime('%b_%d_%Y')+'.csv')
             
             cdcmdf_ratio=len(aai[(aai['Facility']=='Correctional Development Center') | (aai['Facility']=='Metro Detention Facility')])/len(aai)
             
@@ -129,9 +135,9 @@ if __name__=='__main__':
                     if len(ice_p.findall(aai['Alert Notice'][n]))>0:
                         icec+=1
 
-            with open(r'../reports/Incarceration_Report_'+starttime.strftime('%b_%d_%Y')+'.tex','w+') as f:
+            with open(r'../reports/Incarceration_Report_'+query_date.strftime('%b_%d_%Y')+'.tex','w+',encoding='utf-8') as f:
                 content=open(config.latex_template).read()
-                content=content.replace(r'<<date>>',starttime.strftime('%B %d, %Y'))
+                content=content.replace(r'<<date>>',query_date.strftime('%B %d, %Y'))
                 content=content.replace(r'<<Ninmates>>','{:,}'.format(len(aai)))
                 content=content.replace(r'<<Ncharges>>','{:,}'.format(ctd['Misdemeanor']+ctd['Felony']))
                 content=content.replace(r'<<medNcharges>>',str(np.nanmedian(aai['Number of Charges'])))
@@ -146,8 +152,8 @@ if __name__=='__main__':
                 content=content.replace(r'<<plot\_outliers>>',str(config.plot_outliers))
                 content=content.replace(r'<<plot\_other>>',str(config.plot_other*100))
                 content=content.replace(r'<<plot\_other>>',str(config.plot_other*100))
-                content=content.replace(r'<<aaifsize>>',str(os.path.getsize('../data/active_inmates_'+starttime.strftime('%b_%d_%Y')+'.csv')/1e3)+' KB')
-                content=content.replace(r'<<rbfsize>>',str(os.path.getsize('../data/recent_bookings_'+starttime.strftime('%b_%d_%Y')+'.csv')/1e3)+' KB')
+                content=content.replace(r'<<aaifsize>>',str(os.path.getsize('../data/active_inmates_'+query_date.strftime('%b_%d_%Y')+'.csv')/1e3)+' KB')
+                content=content.replace(r'<<rbfsize>>',str(os.path.getsize('../data/recent_bookings_'+query_date.strftime('%b_%d_%Y')+'.csv')/1e3)+' KB')
                 
                 if config.data_web_directory == None:
                     content=content.replace(r'<<dwd>>','')
@@ -156,9 +162,9 @@ if __name__=='__main__':
                 
                 f.write(content)
             
-            os.system(r'pdflatex -output-directory ../reports/ ../reports/Incarceration_Report_'+starttime.strftime('%b_%d_%Y')+'.tex')
+            os.system(r'pdflatex -output-directory ../reports/ ../reports/Incarceration_Report_'+query_date.strftime('%b_%d_%Y')+'.tex')
         except Exception as e:
-            Error_Log('Writing Report',traceback.format_exc(),starttime,r'../misc/error.txt')
+            Error_Log('Writing Report',traceback.format_exc(),query_date,r'../misc/error.txt')
             raise
             
     if config.Email_Noti=='Y':
@@ -173,12 +179,12 @@ if __name__=='__main__':
                     with open(config.email_recipients) as f:
                         addresses = [address.rstrip() for address in f]
                         if len(addresses) > 0:
-                            Email(config.user_email,password,'Incarceration_Report_'+starttime.strftime('%b_%d_%Y'),['Incarceration_Report_'+starttime.strftime('%b_%d_%Y')+'.tex'],addresses)
+                            Email_PDF(config.user_email,password,'Incarceration_Report_'+query_date.strftime('%b_%d_%Y'),'../reports/Incarceration_Report_'+query_date.strftime('%b_%d_%Y')+'.pdf',addresses)
                         else:
                             print('There are no email recipients in recipients.txt to send emails to...')
   
         except Exception as e:
-            Error_Log('Emailing Notifications',traceback.format_exc(),starttime,r'../misc/error.txt')
+            Error_Log('Emailing Notifications',traceback.format_exc(),query_date,r'../misc/error.txt')
             raise e
      
     elif config.Email_Noti=='N':
@@ -186,7 +192,6 @@ if __name__=='__main__':
         
     else:
         error='No Y/N indicated whether to Email Noti in config file. Killing program, please check config.py...'
-        Error_Log('Emailing Notifications',error,starttime,r'../misc/error.txt')
+        Error_Log('Emailing Notifications',error,query_date,r'../misc/error.txt')
     
     print('Code Complete! ','Runtime: ',round((datetime.now(pytz.timezone(config.timezone))-starttime).seconds/60,2),' min')
-    
